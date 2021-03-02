24f91da1e93cb309135c6ef3dd915307a305043e
kylewill0725 <kylewill0725@outlook.com>
2021-03-01 19:10:47 -0600

    Clean start on EliteAPI because the source has loads of code smell

--- EliteSharp/Event/Handler/IEventHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿using EliteSharp.Event.Models.Abstractions;
-
-namespace EliteSharp.Event.Handler
-{
-    public partial interface IEventHandler
-    {
-        void InvokeAllEvent(EventBase eventBase);
-    }
-}
